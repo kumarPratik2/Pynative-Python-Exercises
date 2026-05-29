2fe9053be94324fd8f1eb3e4817df7986dfd7332
Pratik Kumar <pratiktkumar@gmail.com>
2026-05-29 16:31:02 +0530

    Add files via upload

---
+++ Python Basic Exercise for Beginners/Exercise 3.py
@@ -0,0 +1,3 @@
+#Display only those characters which are present at an even index number in given string.
+x = input('Enter a string: ')
+print(x[1::2]) #[start:stop:steps]=> code starts with character at index 1 and goes till end while printing every second character

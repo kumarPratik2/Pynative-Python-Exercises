b02b908817aedfbe19cfd1ce7a91a6b6c368c658
Pratik Kumar <pratiktkumar@gmail.com>
2026-06-15 09:41:21 +0530

    Add files via upload

---
+++ Python Basic Exercise for Beginners/Exercise 33.py
@@ -0,0 +1,6 @@
+#Ask the user for a sentence. Replace every empty space in that sentence with an underscore (_) and print the final result.
+
+message = "I love coding in Python"
+
+new = message.replace(' ', '_')
+print(new)

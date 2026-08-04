669363ee0388e29c63bf179d938971698170acef
Pratik Kumar <pratiktkumar@gmail.com>
2026-08-04 20:47:58 +0530

    feat:accepts two integers and prints its product

---
+++ Python Input and Output Exercise/Exercise 01.py
@@ -0,0 +1,8 @@
+#Write a program that accepts two integer numbers from the user and calculates their multiplication. Print the final result to the console.
+
+First_number = int(input('Enter first number: '))
+Second_number = int(input('Enter second number: '))
+
+product = First_number * Second_number
+
+print(f'Product of {First_number} and {Second_number} is {product}.')

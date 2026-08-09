40fb8b415e059da5e5c7a9a08852de72be576db4
Pratik Kumar <pratiktkumar@gmail.com>
2026-08-09 22:22:13 +0530

    feat:right aligning a string

---
+++ Python Input and Output Exercise/Exercise 09.py
@@ -0,0 +1,6 @@
+#Ask the user for a word and a number. Print the word right-aligned in a total field width of 20 characters, followed by the number.
+
+Word = 'Python'
+Number = 3.12
+
+print(f'{Word:>20} {Number}')

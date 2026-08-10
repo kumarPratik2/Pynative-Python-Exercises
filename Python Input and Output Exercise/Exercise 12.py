d93630f391925f2fb9fd4e0baea45469e5a22b85
Pratik Kumar <pratiktkumar@gmail.com>
2026-08-10 23:03:38 +0530

    feat:using format() to neatly display result

---
+++ Python Input and Output Exercise/Exercise 12.py
@@ -0,0 +1,4 @@
+#Given three variables quantity = 3, totalMoney = 450, and price = 150, use the format() method to print a sentence that neatly displays these values.
+
+result = 'I have {total_money} dollars so I can buy {quantity} football for {price} dollars.'.format(total_money = '450', quantity = '3', price = '150.00')
+print(result)

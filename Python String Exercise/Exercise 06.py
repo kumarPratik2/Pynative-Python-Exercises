cf3ece6b1ded8b8a73da4c5d675f117846db11f0
Pratik Kumar <pratiktkumar@gmail.com>
2026-09-16 19:09:05 +0530

    feat: locate last occurrence of substring using rfind method

---
+++ Python String Exercise/Exercise 06.py
@@ -0,0 +1,5 @@
+#Write a program to find the last index of the substring “Emma” in a given string.
+
+str1 = 'Emma is a data scientist who knows Python. Emma works at google.'
+
+print(f'Last occurence of Emma is starts at index {str1.rfind('Emma')}')
